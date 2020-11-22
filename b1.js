220c74d8a49439be3b66ffd912691c38c02392b5
chhml <2485417621@qq.com>
2020-11-22 09:43:00 +0800

    biiii

--- b1.js
+++ b1.js
@@ -1,4 +1,4 @@
-console.log("这是在文件2的b分支里面写的b1222226666666666666jstttttttttt")
+console.log("这是在文件2的b分支里面写的b1222226666666666666jstt999iiiiiii")
 console.log("这是在文件2的b分支里面写的b1js")
 console.log("这是b的内容1111")
 console.log("这是b的内容999999999999999")
